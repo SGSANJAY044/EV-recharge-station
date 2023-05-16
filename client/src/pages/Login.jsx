1bbdf54468f7cb5ae04f0f8bbf6f0414a51336bf
SGSANJAY044 <sgsanjay044@gmail.com>
2023-05-16 22:47:25 +0530

    login-style

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import car from '../assets/images/login.png'
 function Login() {
     const navigate=useNavigate();
     const [email,setEmail]=useState('');
@@ -24,15 +25,22 @@ function Login() {
           
     }
     return (
-        <>
+        <div className='flex w-screen h-screen overflow-y-hidden'>
+        <div className=' w-1/2 h-full'>
+            <img src={car} alt=""  className='my-32 absolute h-3/5'/>
+            <div className='h-full w-1/3 bg-[#4461F2]'/>
+        </div>
+        <div className='flex w-1/2 h-full items-center justify-center'>
         <form >
-            Email<input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+            <input type="text" value={email} placeholder='Enter Email' onChange={e => setEmail(e.target.value)} className='bg-[#EAF0F7] text-xl px-10 py-3 my-4 rounded-lg w-full focus:outline-none'/>
             <br />
-            Password<input type="text" value={password} onChange={e => setPassword(e.target.value)} />
+            <input type="text" value={password} placeholder='Password' onChange={e => setPassword(e.target.value)} className='bg-[#EAF0F7] text-xl px-10 py-3 my-4 rounded-lg focus:outline-none'/>
             <br/>
-            <input type="button" value="login" onClick={handleSubmit} />
+            <input type="button" value="login"  onClick={handleSubmit} className='bg-[#4461F2] text-xl px-6 py-3 font-bold my-12 rounded-lg w-full text-white shadow-xl cursor-pointer hover:shadow-[#4461f280]'/>
+            <div className='text-[#4461F2] w-full text-center'>new? register now</div>
         </form>
-        </>
+        </div>
+        </div>
     )
 }
 
